2b36d0ed01adf4626d3d716919882c0d92a6e0e4
hugeiemon <knight_of_cups_123@yahoo.co.jp>
2022-05-25 23:05:18 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,27 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index]
+  
   def index
+    @items = Item.all.order('created_at DESC')
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+  
+ 
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :about_item, :category_id, :shipping_cost_id, :condition_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
