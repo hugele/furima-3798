13525d1f0f4dc07335007998ff10a38fb16f1859
hugeiemon <knight_of_cups_123@yahoo.co.jp>
2022-05-31 11:23:51 +0900

    バリテーションの変更

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,21 +1,32 @@
-function price() {
-  const priceInput = document.getElementById("item-price");
-  const add_tax = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
-   priceInput.addEventListener("keyup", () => {
-    const value = priceInput.value;
+// function price() {
+//   const priceInput = document.getElementById("item-price");
+//   const add_tax = document.getElementById("add-tax-price");
+//   const profit = document.getElementById("profit");
+//    priceInput.addEventListener("keyup", () => {
+//     const value = priceInput.value;
 
-    if (value >= 300 && value <= 9999999) {
-      let fee = value * 0.1;
-      let gains = value - fee;
-      add_tax.textContent = Math.floor(fee);
-      profit.textContent = Math.floor(gains);
-    } else {
-      let fee = " ";
-      let gains = " ";
-      add_tax.textContent = fee;
-      profit.textContent = gains;
-    }
-  });
-}
-window.addEventListener("load", price);
+//     if (value >= 300 && value <= 9999999) {
+//       let fee = value * 0.1;
+//       let gains = value - fee;
+//       add_tax.textContent = Math.floor(fee);
+//       profit.textContent = Math.floor(gains);
+//     } else {
+//       let fee = " ";
+//       let gains = " ";
+//       add_tax.textContent = fee;
+//       profit.textContent = gains;
+//     }
+//   });
+// }
+// window.addEventListener("load", price);
+
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    const salesProfit = document.getElementById("profit");
+    salesProfit.innerHTML = inputValue - addTaxDom.innerHTML;
+  })
+});
--- app/models/item.rb
+++ app/models/item.rb
@@ -29,6 +29,6 @@ class Item < ApplicationRecord
       validates :prefecture_id
       validates :condition_id 
     end
-    validates :price, numericality: { only_integer:true, greater_than_or_equal_to:300, less_than: 9999999, message: 'Out of setting range' }
+    validates :price, numericality: { only_integer:true, greater_than_or_equal_to:300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
 
 end
